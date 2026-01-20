3de8eb161c170613d1b970d757e24f397d2b9f42
kushagra4321gkp-droid <kushagra4321gkp@gmail.com>
2026-01-20 20:36:07 +0530

    Servlet Mapping

--- src/main/java/com/example/demo/App.java
+++ src/main/java/com/example/demo/App.java
@@ -1,13 +1,49 @@
 package com.example.demo;
 
+import org.apache.catalina.Context;
+import org.apache.catalina.LifecycleException;
+import org.apache.catalina.startup.Tomcat;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
+    public static void main( String[] args ) throws LifecycleException {
         System.out.println( "Hello World!" );
+
+        Tomcat tomcat = new Tomcat();
+
+        // 1️⃣ Set port
+        tomcat.setPort(8080);
+
+        // 2️⃣ Create context
+        Context context = tomcat.addContext("", null);
+
+        // 3️⃣ Register servlet
+        Tomcat.addServlet(context, "helloServ", new HelloServlet());
+        context.addServletMappingDecoded("/hello", "helloServ");
+
+        // 4️⃣ FORCE connector creation (VERY IMPORTANT)
+        tomcat.getConnector();
+
+        // 5️⃣ Start Tomcat
+        tomcat.start();
+
+        // 6️⃣ KEEP JVM ALIVE
+        tomcat.getServer().await();
+
+
+
+        //
+//        Context context = tomcat.addContext("", null);
+//        Tomcat.addServlet(context ,"helloServ", new HelloServlet());
+//        context.addServletMappingDecoded("/hello", "helloServ");
+//        tomcat.start();
+//        tomcat.getServer().await();
+
+
+
     }
 }
