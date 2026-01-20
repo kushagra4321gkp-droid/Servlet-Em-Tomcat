f154c736676ea34526b74b389bfb0e4950ab8d0b
kushagra4321gkp-droid <kushagra4321gkp@gmail.com>
2026-01-20 21:14:49 +0530

    Responding to Client using PrintWriter's object which accept the response the getWriter method of HttpServletResponse object -> in one class you do get request, process it and respond which becomes nightmare in big applications that is solved in SpringMVC

--- src/main/java/com/example/demo/HelloServlet.java
+++ src/main/java/com/example/demo/HelloServlet.java
@@ -3,11 +3,18 @@ package com.example.demo;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 public class HelloServlet extends HttpServlet {
 
-    public void service(HttpServletRequest req, HttpServletResponse res){
+    public void service(HttpServletRequest req, HttpServletResponse res) throws IOException {
         System.out.println("Inside servlet");
+
+        res.setContentType("text/html");
+        PrintWriter out = res.getWriter();
+//        res.getWriter().println("<h2><b>Hello World</b></h2>");
+        out.println("<h2><b>Hello World</b></h2>");
     }
 
 }
